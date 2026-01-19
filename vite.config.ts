c40d33e2daee6dc6681dc32328b9cf73f8f33b80
IuraCPersonal <47008227+IuraCPersonal@users.noreply.github.com>
2026-01-19 11:32:40 +0200

    feat: add shadcn and tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 import { crx } from "@crxjs/vite-plugin";
@@ -6,10 +8,15 @@ import manifest from "./manifest.config";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [react(), crx({ manifest }), tailwindcss()],
   server: {
     cors: {
       origin: [/chrome-extension:\/\//],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
