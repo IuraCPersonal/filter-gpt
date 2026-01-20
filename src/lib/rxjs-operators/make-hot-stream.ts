4f8a6723742ea6e0bf81d9ec5c6d5e1d056041af
IuraCPersonal <47008227+IuraCPersonal@users.noreply.github.com>
2026-01-20 14:20:04 +0200

    docs: document makeHotStream util

--- src/lib/rxjs-operators/make-hot-stream.ts
+++ src/lib/rxjs-operators/make-hot-stream.ts
@@ -1,6 +1,10 @@
 import { Observable, ReplaySubject, connectable } from "rxjs";
 
-export const makeHotStream = <T>(observable: Observable<T>) => {
+/**
+ * Converts a cold RxJS observable into a hot observable stream
+ * using a ReplaySubject(1) to ensure all subscribers receive the latest value.
+ */
+export const makeHotStream = <T>(observable: Observable<T>): Observable<T> => {
   const hot$ = connectable(observable, {
     connector: () => new ReplaySubject(1),
   });
